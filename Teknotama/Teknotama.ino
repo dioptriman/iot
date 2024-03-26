501db87d1f289e2d82c1d5b95f172a7f1048320e
Andhika Agung Pratama <79739062+dioptriman@users.noreply.github.com>
2024-03-27 00:54:44 +0700

    change to the ino

--- Teknotama/Teknotama.ino
+++ Teknotama/Teknotama.ino
@@ -5,52 +5,31 @@
 #include "SPIFFS.h"
 #include <Arduino_JSON.h>
 
-
 #define timeSeconds 1
 
-
 // Replace with your network credentials
-const char* ssid = "Andika";
-const char* password = "30032003";
+const char* ssid = "YourSSID";
+const char* password = "YourPassword";
 
-// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
-
 AsyncWebSocket ws("/ws");
 
-// Json Variable to Hold Sensor Readings
 JSONVar readings;
 
-// Timer variables
-unsigned long lastTime = 0;
-unsigned long timerDelay = 30;
-
-//PIR Sensor
 const int PIN_TO_SENSOR = 16;
-
-//Door Sensor
 const int DOOR_SENSOR_PIN = 18;
-
-
-//Relay
 const int relay = 36;
 
-//Door State
 int doorState;
+bool relayState = false; // Initialize relay state
 
-bool ledState = 0;
-
-
-
-String getJsonData(){
-  readings["pir_sensor"] = String(digitalRead(DOOR_SENSOR_PIN));
-  readings["door_sensor"] = String(digitalRead(PIN_TO_SENSOR));
+String getJsonData() {
+  readings["pir_sensor"] = String(digitalRead(PIN_TO_SENSOR));
+  readings["door_sensor"] = String(digitalRead(DOOR_SENSOR_PIN));
   String jsonString = JSON.stringify(readings);
   return jsonString;
 }
 
-
-// Initialize SPIFFS
 void initSPIFFS() {
   if (!SPIFFS.begin(true)) {
     Serial.println("An error has occurred while mounting SPIFFS");
@@ -58,7 +37,6 @@ void initSPIFFS() {
   Serial.println("SPIFFS mounted successfully");
 }
 
-// Initialize WiFi
 void initWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -70,41 +48,27 @@ void initWiFi() {
   Serial.println(WiFi.localIP());
 }
 
-void notifyClients(String getJsonData) {
-  ws.textAll(getJsonData);
+void notifyClients(String jsonData) {
+  ws.textAll(jsonData);
 }
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-    try {
-      var myObj = JSON.parse(data);
-      var keys = Object.keys(myObj);
-
-      // Check for sensor data identifier (optional)
-      if (strcmp((char*)data, "getReadings") == 0) {
-        for (var i = 0; i < keys.length; i++) {
-          var key = keys[i];
-          document.getElementById(key).innerHTML = myObj[key];
-        }
-      } else {
-        // Handle relay data (assuming a simple string like "1" or "0")
-        console.log(data);
-        var state;
-        if (data == "1") {
-          state = "ON";
-        } else {
-          state = "OFF";
-        }
-        document.getElementById('state').innerHTML = state;
-      }
-    } catch(error) {
-      console.error("Error parsing data:", error);
-      // Handle parsing error (optional)
+    char message[len + 1];
+    memcpy(message, data, len);
+    message[len] = '\0';
+    if (strcmp(message, "toggle") == 0) {
+      relayState = !relayState;
+      digitalWrite(relay, relayState); // Toggle relay state
+      notifyClients(getJsonData()); // Notify clients with updated sensor readings
+    } else if (strcmp(message, "getReadings") == 0) {
+      String jsonData = getJsonData();
+      Serial.println(jsonData);
+      notifyClients(jsonData);
     }
   }
 }
-
 
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
   switch (type) {
@@ -128,13 +92,12 @@ void initWebSocket() {
   server.addHandler(&ws);
 }
 
-String processor(const String& var){
+String processor(const String& var) {
   Serial.println(var);
-  if(var == "STATE"){
-    if (ledState){
+  if (var == "STATE") {
+    if (relayState) {
       return "ON";
-    }
-    else{
+    } else {
       return "OFF";
     }
   }
@@ -142,26 +105,22 @@ String processor(const String& var){
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(DOOR_SENSOR_PIN, INPUT_PULLUP);
   pinMode(PIN_TO_SENSOR, INPUT);
   pinMode(relay, OUTPUT);
-
   digitalWrite(relay, LOW);
 
   initWiFi();
   initSPIFFS();
   initWebSocket();
 
-  // Web Server Root URL
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/index.html", "text/html");
+    request->send(SPIFFS, "/index.html", "text/html", processor);
   });
 
   server.serveStatic("/", SPIFFS, "/");
 
-  // Start server
   server.begin();
 }
 
@@ -169,15 +128,12 @@ void loop() {
   doorState = digitalRead(DOOR_SENSOR_PIN);
   int pirState = digitalRead(PIN_TO_SENSOR);
 
-  if((doorState == HIGH) and (pirState == LOW)){
+  // Control relay based on sensor states
+  if (doorState == HIGH && pirState == LOW) {
     relayState = true;
-    } 
-    else if ((doorState == LOW) and (pirState == HIGH)){
-     relayState = false;
-    }
+  } else if (doorState == LOW && pirState == HIGH) {
+    relayState = false;
+  }
 
-  ws.cleanupClients();
-
-  digitalWrite(relay, relayState);
-
+  ws.cleanupClients(); // Cleanup WebSocket clients
 }
